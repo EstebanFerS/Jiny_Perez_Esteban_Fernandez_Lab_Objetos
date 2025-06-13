3ccd056ce5cb53aee8ae2a73a911c665193b8ced
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-06-13 13:11:54 -0600

    Primera Carga

---
+++ Jiny_Perez_Esteban_Fernandez_Lab_Objetos/src/jiny_perez_esteban_fernandez_lab_objetos/Jiny_Perez_Esteban_Fernandez_Lab_Objetos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jiny_perez_esteban_fernandez_lab_objetos;
+
+/**
+ *
+ * @author esteb
+ */
+public class Jiny_Perez_Esteban_Fernandez_Lab_Objetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
